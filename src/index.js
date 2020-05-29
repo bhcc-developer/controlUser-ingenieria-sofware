58d9fe62df98fcf61f71ddf4d6aae394f0d7b787
Bryan Carias <bcariascc@outlook.com>
2020-05-29 11:37:28 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const app = express()
 
 app.set('port', process.env.PORT || 4000)
 
+app.use( cors() )
 require('./database')
 app.use( express.json() )
 app.use( express.urlencoded({extended:false}))
-app.use( cors() )
 
 app.use( require('./router/user') )
 
